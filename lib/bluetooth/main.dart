355aaa3420681f982c70d1f0878654a9eaf7fda9
david tsai <david90103@gmail.com>
2019-10-09 14:33:50 +0800

    init

---
+++ lib/bluetooth/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import './MainPage.dart';
+
+void main() => runApp(new ExampleApplication());
+
+class ExampleApplication extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainPage()
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+/// Line chart example
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:flutter/material.dart';
+import 'bluetooth/main.dart';
+import 'home_page.dart';
+import 'login.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final routes = <String, WidgetBuilder>{
+    LoginPage.tag: (context) => LoginPage(),
+    HomePage.tag: (context) => HomePage(),
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kodeversitas',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+        fontFamily: 'Nunito',
+      ),
+      home: LoginPage(),
+      routes: routes,
+    );
+  }
+}
